6a0d3251f8bb83a1753ebab2850e7e5711ea6678
ErlanggaIlkom <115127466+ErlanggaIlkom@users.noreply.github.com>
2022-11-24 11:30:04 +0700

    Create Variabel.java

---
+++ Variabel.java
@@ -0,0 +1,19 @@
+class uts_alpro {
+  public static void main(String args[]){ 
+    string a;
+    string nama;
+    int umur;
+    int lahir;
+    
+    a = "assalamualaikum wr wb";
+    nama = "Muhammad Erlangga Gunawan";
+    umur = 19;
+    lahir = 2003;
+    
+    System.out.print(a);
+    Syste.out.print("Nama Saya = " + nama);
+    System.out.print("Umur Saya = " + umur);
+    System.out.print("Kelahiran = " + lahir);
+  }
+}
+   
