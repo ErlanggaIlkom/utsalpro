2645c35416c3734a052cdcce5433e5282380ff7d
ErlanggaIlkom <115127466+ErlanggaIlkom@users.noreply.github.com>
2022-11-24 13:32:10 +0700

    Update Variabel.java

--- Variabel.java
+++ Variabel.java
@@ -5,15 +5,15 @@ class uts_alpro {
     int umur;
     int lahir;
     
-    a = "assalamualaikum wr wb";
+    a = "assalamual'aikum wr wb";
     nama = "Muhammad Erlangga Gunawan";
     umur = 19;
     lahir = 2003;
     
-    System.out.print(a);
-    Syste.out.print("Nama Saya = " + nama);
-    System.out.print("Umur Saya = " + umur);
-    System.out.print("Kelahiran = " + lahir);
+    System.out.println(a);
+    Syste.out.println("Nama Saya = " + nama);
+    System.out.println("Umur Saya = " + umur);
+    System.out.println("Saya lahir di tahun = " + lahir);
   }
 }
    
